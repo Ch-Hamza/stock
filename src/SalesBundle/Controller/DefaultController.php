a711954bf02020ba18cbac89fd676c4bbe872b80
Hamza Chebil <hamzachebil40@gmail.com>
2018-08-19 19:35:58 +0100

    save sales done

--- src/SalesBundle/Controller/DefaultController.php
+++ src/SalesBundle/Controller/DefaultController.php
@@ -2,17 +2,37 @@
 
 namespace SalesBundle\Controller;
 
+use ProductBundle\Entity\Product;
+use SalesBundle\Entity\Sale;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/save-sale", name="save_sale_page")
      */
-    public function SaveSaleAction()
+    public function SaveSaleAction(Request $request)
     {
-        //if post save sale then redirect to index
-        //else redirect to index
+        if($request->isMethod('POST')) {
+            $data = $request->request->get('items');
+            $array_data = json_decode($data, true);
+            $em = $this->getDoctrine()->getManager();
+            foreach ($array_data as $item)
+            {
+                $sale = new Sale();
+                $product = $em->getRepository(Product::class)->find($item['item']['id']);
+                $sale->setProduct($product);
+                $sale->setQuantity($item['sale_quantity']);
+                $sale->setSaleDate(new \DateTime('now'));
+                $em->persist($sale);
+                $em->flush();
+            }
+            return $this->redirectToRoute('index_page');
+        }
+        return $this->redirectToRoute('index_page');
     }
 }
